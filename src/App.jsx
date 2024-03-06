c85623b76a3257c10dcd010cfedf90e9ac7f1cf8
Facundo Cozzani <fcozzani@cafemartinez.central>
2024-03-06 16:13:13 -0300

    Carrusel + Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { Carrusel } from './Carrusel'
+
 function App () {
   return (
     <>
+      <main>
+        <Carrusel />
+        <header className='container d-flex'>
+          <figure className='figure'>
+            <img src='/moliLogo.png' className='figure-img h-100 w-100 rounded' alt='logo.png' />
+          </figure>
 
+          <div>
+            <h1 className='fs-3 lh-1'>Moli Ahumado</h1>
+            <span className='lh-1'>Carnes Ahumadas</span>
+          </div>
+        </header>
+        <hr className='container w-75' />
+        <div className='container d-flex justify-content-around'>
+          <a href=''><i class='bi bi-whatsapp' /></a>
+          <a href=''><i class='bi bi-telephone' /></a>
+          <a href=''><i class='bi bi-share' /></a>
+        </div>
+      </main>
     </>
   )
 }
--- src/Carrusel.jsx
+++ src/Carrusel.jsx
@@ -1,7 +1,23 @@
 export function Carrusel () {
   return (
     <>
+      <div id='carouselExampleInterval' className='carousel slide' data-bs-ride='carousel'>
+        <div className='carousel-inner'>
 
+          <div className='carousel-item active' data-bs-interval='4000'>
+            <img src='/SANGUCHES-1.jpg' className='d-block w-100' alt='SANGUCHES-1.jpg' />
+          </div>
+
+          <div className='carousel-item' data-bs-interval='4000'>
+            <img src='/SANGUCHES-2.jpg' className='d-block w-100' alt='SANGUCHES-2.jpg' />
+          </div>
+
+          <div className='carousel-item'>
+            <img src='/SANGUCHES-3.jpg' className='d-block w-100' alt='SANGUCHES-3.jpg' />
+          </div>
+
+        </div>
+      </div>
     </>
   )
 }
