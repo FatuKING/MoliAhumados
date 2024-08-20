364c5c7b4b5ead292b588536027b13fceafead31
Facundo Cozzani <cozzanifacundo@gmail.com>
2024-08-20 14:13:17 -0300

    Agregamos opcion delivery

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -6,12 +6,16 @@ export function Cart () {
   const { state, increaseItemQuantity, decreaseItemQuantity, resetToCart, closeCart, deleteItemCart, subTotal } = useCart()
   const idCheckboxCash = useId()
   const idCheckboxMP = useId()
+  const idCheckTakeAway = useId()
+  const idCheckDelivery = useId()
 
   const cashRef = useRef(null)
+  const deliveryRef = useRef(null)
 
   const handleFinalizeOrder = () => {
     const selectedPaymentMethod = cashRef.current.checked ? 'Efectivo' : 'Mercado Pago'
-    finalizeOrder(state, selectedPaymentMethod)
+    const selectedDeliveryMethod = deliveryRef.current.checked ? 'Take Away' : 'Delivery'
+    finalizeOrder(state, selectedPaymentMethod, selectedDeliveryMethod)
   }
 
   return (
@@ -19,29 +23,29 @@ export function Cart () {
       <div className='cart'>
         <main className='cartMain'>
           <header className='container d-flex justify-content-between align-items-center' style={{ height: '100px' }}>
-            <button className='btn btn-sm' onClick={closeCart}>
+            <button className='btn btn-sm p-0' onClick={closeCart}>
               <i className='bi bi-arrow-left' style={{ fontSize: '24px' }} />
             </button>
 
-            <h1 className='fw-semibold fs-4'>Detalles del Pedido</h1>
+            <h1 className='fw-semibold fs-5'>Detalles del Pedido</h1>
           </header>
 
-          <h2 className='bg-dark text-white text-center fw-semibold p-2 text-uppercase m-0 fs-5'>Productos</h2>
+          <h2 className='bg-dark text-white text-center fw-semibold p-2 text-uppercase m-0 fs-6'>Productos</h2>
           <section className='d-flex flex-column'>
             {
                 state.map((products, index) => {
                   return (
                     <article key={index} className='d-flex justify-content-between align-items-center' style={{ height: '100px' }}>
-                      <img src={products.img} alt='' className='cartImg' />
+                      <img src={products.img} alt={products.img} className='cartImg' />
 
-                      <div className='d-flex justify-content-start' style={{ width: '120px' }}>
-                        <h3 className='fs-6 fw-semibold'>{products.product}</h3>
+                      <div className='d-flex justify-content-center' style={{ width: '120px' }}>
+                        <h3 className='cartText fw-semibold'>{products.product}</h3>
                       </div>
 
-                      <div className='d-flex justify-content-between align-items-center' style={{ width: '100px' }}>
-                        <button className='btn btn-sm' onClick={() => decreaseItemQuantity(products)}><i className='bi bi-dash' /></button>
+                      <div className='d-flex justify-content-center gap-2 align-items-center' style={{ width: '100px' }}>
+                        <button className='btn btn-sm p-1' onClick={() => decreaseItemQuantity(products)}><i className='bi bi-dash' /></button>
                         {products.quantity}
-                        <button className='btn btn-sm' onClick={() => increaseItemQuantity(products)}><i className='bi bi-plus' /></button>
+                        <button className='btn btn-sm p-1' onClick={() => increaseItemQuantity(products)}><i className='bi bi-plus' /></button>
                       </div>
 
                       <span style={{ width: '100px' }}>${products.priceEnd}</span>
@@ -55,7 +59,22 @@ export function Cart () {
               <button className='btn btn-link text-black' onClick={resetToCart}>Limpiar Carrito</button>
             </div>
             <article className='d-flex flex-column pt-4'>
-              <h2 className='bg-dark text-white text-center fw-semibold p-2 text-uppercase fs-5'>Forma de pago</h2>
+              <h2 className='bg-dark text-white text-center fw-semibold p-2 text-uppercase fs-6'>Forma de entrega</h2>
+              <form className='d-flex flex-column container'>
+                <label className='fs-5 labelCheck' htmlFor={idCheckTakeAway}>
+                  <input ref={deliveryRef} type='radio' id={idCheckTakeAway} name='deliveryMethod' /> <span>Take Away</span>
+                  <span className='checkmark' />
+                </label>
+
+                <label className='fs-5 labelCheck' htmlFor={idCheckDelivery}>
+                  <input type='radio' id={idCheckDelivery} style={{ height: '1.5rem' }} name='deliveryMethod' /> Delivery
+                  <span className='checkmark' />
+                </label>
+              </form>
+            </article>
+
+            <article className='d-flex flex-column pt-4'>
+              <h2 className='bg-dark text-white text-center fw-semibold p-2 text-uppercase fs-6'>Forma de pago</h2>
               <form className='d-flex flex-column container'>
                 <label className='fs-5 labelCheck' htmlFor={idCheckboxCash}>
                   <input ref={cashRef} type='radio' id={idCheckboxCash} name='paymentMethod' /> <span>Efectivo</span>
--- src/logic/comandWpp.js
+++ src/logic/comandWpp.js
@@ -1,12 +1,13 @@
 const phoneNumber = '5491124037996'
 
-const generateMessage = (cart, paymentType) => {
-  let message = '*Hola, quiero realizar el siguiente pedido:*\n'
+const generateMessage = (cart, paymentType, deliveryType) => {
+  let message = '*Hola, quiero realizar el siguiente pedido:*\n\n'
 
   cart.forEach(item => {
-    message += `*- ${item.product} x ${item.quantity} ($${item.priceEnd} c/u)*\n`
+    message += `*- ${item.product} x ${item.quantity} ($${item.priceEnd})*\n`
   })
 
+  message += `\n*Forma de entrega: ${deliveryType}*`
   message += `\n*Forma de pago: ${paymentType}*\n`
   message += `\n*Total: ${calculateTotal(cart)}*\n`
 
@@ -17,8 +18,8 @@ function calculateTotal (cart) {
   return cart.reduce((accumulator, item) => accumulator + Number(item.priceEnd), 0).toLocaleString('es-ES')
 }
 
-export function finalizeOrder (cart, paymentType) {
-  const mensaje = generateMessage(cart, paymentType)
+export function finalizeOrder (cart, paymentType, deliveryType) {
+  const mensaje = generateMessage(cart, paymentType, deliveryType)
   const encodedMessage = encodeURIComponent(mensaje)
   const whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`
   window.location.href = whatsappURL
