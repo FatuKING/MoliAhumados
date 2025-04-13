eae4b611f841b694aff03e755c3d99cc15c11a86
Fatu <cozzanifacundo@gmail.com>
2025-04-12 22:56:13 -0300

    Style: Modifico estilos del carrito

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,10 @@
 import { useId, useState } from 'react'
 import { useCart } from '../hooks/useCart.js'
 import { finalizeOrder } from '../logic/comandWpp.js'
+import { Link } from 'react-router-dom'
 
 export function Cart () {
-  const { state, increaseItemQuantity, decreaseItemQuantity, resetToCart, closeCart, deleteItemCart } = useCart()
+  const { state, increaseItemQuantity, decreaseItemQuantity, resetToCart, deleteItemCart } = useCart()
   const idCheckboxCash = useId()
   const idCheckboxMP = useId()
   const idCheckTakeAway = useId()
@@ -48,14 +49,14 @@ export function Cart () {
       <div className='cart'>
         <main className='cartMain'>
           <header className='container d-flex justify-content-between align-items-center' style={{ height: '100px' }}>
-            <button className='btn btn-sm p-0' onClick={closeCart}>
+            <Link to='/' className='btn btn-sm p-0'>
               <i className='bi bi-arrow-left' style={{ fontSize: '24px' }} />
-            </button>
+            </Link>
 
             <h1 className='fw-semibold fs-5'>Detalles del Pedido</h1>
           </header>
 
-          <h2 className='bg-dark text-white text-center fw-semibold p-2 text-uppercase m-0 fs-6'>Productos</h2>
+          <h2 className=' fw-semibold p-2 m-0 fs-6'>Productos</h2>
           <section className='d-flex flex-column'>
             {
                 state.map((products, index) => {
@@ -85,9 +86,12 @@ export function Cart () {
               <button className='btn btn-link text-black' onClick={resetToCart}>Limpiar Carrito</button>
             </div>
 
-            <article className='d-flex flex-column pt-4'>
-              <h2 className='bg-dark text-white text-center fw-semibold p-2 text-uppercase fs-6'>Forma de entrega</h2>
               <form className='d-flex flex-column container' onSubmit={handleFinalizeOrder}>
+                <section className='d-flex justify-content-between align-items-center'>
+
+                <article className='d-flex flex-column pt-2'>
+                <h2 className='fw-semibold m-0 fs-6'>Forma de entrega</h2>
+
                 <label className='fs-5 labelCheck' htmlFor={idCheckTakeAway}>
                   <input type='radio' id={idCheckTakeAway} name='deliveryMethod' onClick={handleTakeAwayChange} value='Take Away' /> <span>Take Away</span>
                   <span className='checkmark' />
@@ -102,18 +106,22 @@ export function Cart () {
 
                 {isDelivery && <input type='text' id='address' className='form-control inputCart' name='addressClient' required placeholder='Dirección completa' />}
 
-                <article className='d-flex flex-column pt-2'>
-                  <h2 className='bg-dark text-white text-center fw-semibold p-2 text-uppercase fs-6'>Forma de pago</h2>
                 </article>
-                <label className='fs-5 labelCheck' htmlFor={idCheckboxCash}>
-                  <input type='radio' id={idCheckboxCash} name='paymentMethod' value='Efectivo' required /> <span>Efectivo</span>
-                  <span className='checkmark' />
-                </label>
 
-                <label className='fs-5 labelCheck' htmlFor={idCheckboxMP}>
-                  <input type='radio' id={idCheckboxMP} style={{ height: '1.5rem' }} name='paymentMethod' value='Mercado Pago' required /> Mercado Pago
-                  <span className='checkmark' />
-                </label>
+                <article className='d-flex flex-column pt-2'>
+                  <h2 className='fw-semibold fs-6'>Forma de pago</h2>
+          
+                  <label className='fs-5 labelCheck' htmlFor={idCheckboxCash}>
+                    <input type='radio' id={idCheckboxCash} name='paymentMethod' value='Efectivo' required /> <span>Efectivo</span>
+                    <span className='checkmark' />
+                  </label>
+
+                  <label className='fs-5 labelCheck' htmlFor={idCheckboxMP}>
+                    <input type='radio' id={idCheckboxMP} style={{ height: '1.5rem' }} name='paymentMethod' value='Mercado Pago' required /> Transferencia
+                    <span className='checkmark' />
+                  </label>
+                </article>
+                </section>
 
                 <article className='d-flex flex-column align-items-center pt-5 mb-3'>
                   <div className='d-flex justify-content-between align-items-center pb-1' style={{ width: '60%' }}>
@@ -123,7 +131,6 @@ export function Cart () {
                   <button className='btn btn-dark rounded fw-semibold text-uppercase' type='submit' style={{ width: '60%' }}>Confirmar Pedido</button>
                 </article>
               </form>
-            </article>
           </section>
         </main>
       </div>
