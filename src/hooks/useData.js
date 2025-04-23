cf76647ada4650c251c5865728a36b21cb9b4c15
Fatu <cozzanifacundo@gmail.com>
2025-04-23 11:18:54 -0300

    hostinger una mierda total

--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -6,10 +6,9 @@ export const useData = create((set) => ({
   loading: true, 
   setData: async () => {
     try {
-      const response = await fetch(import.meta.env.VITE_API_PRODUCTS)
+      const response = await fetch("https://sendmailmembranas202.onrender.com/sendMail")
       const data = await response.json()
       const categories = Object.keys(data)
-      console.log(categories)
       set({ data, categories, loading: false }) 
     } catch (error) {
       console.error("Error fetching data:", error)
